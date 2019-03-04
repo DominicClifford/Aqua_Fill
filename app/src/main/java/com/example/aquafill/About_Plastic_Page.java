813005b099edfe23f84a574d242eebb0bff32269
Dominic Clifford <dominic.cliff12@gmail.com>
2019-03-04 15:22:01 +0000

    Addition of individual page resource values

--- app/src/main/java/com/example/aquafill/About_Plastic_Page.java
+++ app/src/main/java/com/example/aquafill/About_Plastic_Page.java
@@ -9,7 +9,7 @@ public class About_Plastic_Page extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about__plastic__page);
+        setContentView(R.layout.about_plastic_page);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("About Plastic");
